454cb5112c845875b97331621afacb3f75c45349
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2022-11-25 11:32:09 +0300

    no message

--- app/src/main/java/com/example/temelkomutlarunite_3/Uyg7Activity.java
+++ app/src/main/java/com/example/temelkomutlarunite_3/Uyg7Activity.java
@@ -1,4 +1,31 @@
 package com.example.temelkomutlarunite_3;
 
-public class Uyg7Activity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Uyg7Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        int x = 10;
+        int y = 3;
+        int toplam = x + y;
+        int fark = x - y;
+        int carpim = x * y;
+        int bolme = x / y;
+        int mod = x % y;
+        x++;
+        y--;
+        System.out.println("Toplam: " + toplam);
+        System.out.println("Fark: " + fark);
+        System.out.println("Çarpım: " + carpim);
+        System.out.println("Bölme: " + bolme);
+        System.out.println("Mod Alma: " + mod);
+        System.out.println("Artırma: " + x);
+        System.out.println("Azaltma: " + y);
+    }
 }
